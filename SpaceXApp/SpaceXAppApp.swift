0abe9ecdb7065f925fbcf707ea5d5267287cfa74
Amit Rawal <mail2amitrawal@gmail.com>
2024-01-13 17:10:39 +0530

    Create Project

---
+++ SpaceXApp/SpaceXAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXAppApp.swift
+//  SpaceXApp
+//
+//  Created by Amit Rawal on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
